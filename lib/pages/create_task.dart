cb09c0678fbc00a70a2a2e6aa1041b56e5aea6fc
Chaitanya Jadhav <chaitanyajadhav634@gmail.com>
2022-08-30 23:02:12 +0530

    created task dialog

--- lib/pages/create_task.dart
+++ lib/pages/create_task.dart
@@ -17,75 +17,106 @@ class _CreateTaskPageState extends State<CreateTaskPage> {
         title: Text('Create Task'),
         actions: [TextButton(onPressed: () {}, child: Text('Save'))],
       ),
-      body: ListView(children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 12, left: 8, right: 8),
-          child: TextField(
-            decoration: InputDecoration(
-                label: Text('Title'),
-                prefixIcon: IconButton(
-                  icon: Icon(
-                    Icons.star_border,
-                    color: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: ListView(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: TextField(
+              decoration: InputDecoration(
+                  label: Text('Title'),
+                  prefixIcon: IconButton(
+                    icon: Icon(
+                      Icons.star_border,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      debugPrint('Pressed');
+                    },
                   ),
-                  onPressed: () {
-                    debugPrint('Pressed');
-                  },
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    Icons.check_box_outline_blank_outlined,
-                    color: Colors.black,
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.check_box_outline_blank_outlined,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {
+                      debugPrint('Pressed');
+                    },
                   ),
-                  onPressed: () {
-                    debugPrint('Pressed');
-                  },
-                ),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(18))),
-          ),
-        ),
-        Column(
-          children: subtask,
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8, top: 4),
-              child: OutlinedButton.icon(
-                onPressed: () {
-                  setState(() {
-                    subtask.add(Padding(
-                      padding: const EdgeInsets.only(left: 54, top: 8, right: 8),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            label: Text('Subtitle'),
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                Icons.check_box_outline_blank_outlined,
-                                color: Colors.black,
-                              ),
-                              onPressed: () {
-                                debugPrint('Pressed');
-                              },
-                            ),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(18))),
-                      ),
-                    ));
-                  });
-                },
-                icon: Icon(Icons.add),
-                label: Text('Subtask'),
-                style:
-                    OutlinedButton.styleFrom(side: BorderSide(color: Colors.red)),
-              ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(18))),
             ),
-          ],
-        )
-      ]),
+          ),
+          Column(
+            children: subtask,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 4),
+                child: OutlinedButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      subtask.add(Padding(
+                        padding: const EdgeInsets.only(left: 46, top: 8,),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              label: Text('Subtitle'),
+                              suffixIcon: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                IconButton(
+                                icon: Icon(
+                                  Icons.check_box_outline_blank_outlined,
+                                  color: Colors.black,
+                                ),
+                                onPressed: () {
+                                  debugPrint('Pressed');
+                                },
+                              ),
+                              IconButton(
+                                icon: Icon(
+                                  Icons.remove,
+                                  color: Colors.black,
+                                ),
+                                onPressed: () {
+                                  debugPrint('Pressed');
+                                },
+                              ),
+                              ]),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(18))),
+                        ),
+                      ));
+                    });
+                  },
+                  icon: Icon(Icons.add),
+                  label: Text('Subtask'),
+                  style:
+                      OutlinedButton.styleFrom(side: BorderSide(color: Colors.red)),
+                ),
+              ),
+              
+            ]
+          ),
+  Divider(indent: 138, endIndent: 138,),
+
+          ListTile(leading: Icon(Icons.notifications_active_outlined), title: Text('Add reaminder')),
+          Divider(indent: 68),
+          ListTile(leading: Icon(Icons.event_outlined), title: Text('Add due date')),
+          Divider(indent: 68),
+          ListTile(leading: Icon(Icons.event_repeat_outlined), title: Text('Repeat')),
+          Divider(indent: 68),
+          Expanded(child: Container(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 18),
+              child: Text('Add Note'),
+            ),
+          ),)
+        ]),
+      ),
     );
   }
 }
