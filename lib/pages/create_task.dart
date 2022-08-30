287f96824c563cad77bf9850ac6a94e76c9e165d
Chaitanya Jadhav <chaitanyajadhav634@gmail.com>
2022-08-31 01:07:34 +0530

    create task dialog is made

--- lib/pages/create_task.dart
+++ lib/pages/create_task.dart
@@ -14,8 +14,10 @@ class _CreateTaskPageState extends State<CreateTaskPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.close_rounded),onPressed: (() => Navigator.pop(context))),
         title: Text('Create Task'),
         actions: [TextButton(onPressed: () {}, child: Text('Save'))],
+        // titleTextStyle: Theme.of(context).appBarTheme.titleTextStyle,
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 8),
@@ -51,70 +53,74 @@ class _CreateTaskPageState extends State<CreateTaskPage> {
             children: subtask,
           ),
           Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 4),
-                child: OutlinedButton.icon(
-                  onPressed: () {
-                    setState(() {
-                      subtask.add(Padding(
-                        padding: const EdgeInsets.only(left: 46, top: 8,),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              label: Text('Subtitle'),
-                              suffixIcon: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                IconButton(
-                                icon: Icon(
-                                  Icons.check_box_outline_blank_outlined,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {
-                                  debugPrint('Pressed');
-                                },
-                              ),
-                              IconButton(
-                                icon: Icon(
-                                  Icons.remove,
-                                  color: Colors.black,
-                                ),
-                                onPressed: () {
-                                  debugPrint('Pressed');
-                                },
-                              ),
-                              ]),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(18))),
-                        ),
-                      ));
-                    });
-                  },
-                  icon: Icon(Icons.add),
-                  label: Text('Subtask'),
-                  style:
-                      OutlinedButton.styleFrom(side: BorderSide(color: Colors.red)),
+              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: OutlinedButton.icon(
+                    onPressed: () {
+                      setState(() {
+                        subtask.add(Padding(
+                          padding: const EdgeInsets.only(
+                            left: 46,
+                            top: 8,
+                          ),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                label: Text('Subtitle'),
+                                suffixIcon: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons
+                                              .check_box_outline_blank_outlined,
+                                          color: Colors.black,
+                                        ),
+                                        onPressed: () {
+                                          debugPrint('Pressed');
+                                        },
+                                      ),
+                                      // IconButton(
+                                      //   icon: Icon(
+                                      //     Icons.remove,
+                                      //     color: Colors.black,
+                                      //   ),
+                                      //   onPressed: () {},
+                                      // ),
+                                    ]),
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(18))),
+                          ),
+                        ));
+                      });
+                    },
+                    icon: Icon(Icons.add),
+                    label: Text('Subtask'),
+                    style: OutlinedButton.styleFrom(
+                        side: BorderSide(color: Colors.red)),
+                  ),
                 ),
-              ),
-              
-            ]
+              ]),
+          Divider(
+            indent: 138,
+            endIndent: 138,
           ),
-  Divider(indent: 138, endIndent: 138,),
-
-          ListTile(leading: Icon(Icons.notifications_active_outlined), title: Text('Add reaminder')),
+          ListTile(
+              leading: Icon(Icons.notifications_active_outlined),
+              title: Text('Add reaminder')),
           Divider(indent: 68),
-          ListTile(leading: Icon(Icons.event_outlined), title: Text('Add due date')),
+          ListTile(
+              leading: Icon(Icons.event_outlined), title: Text('Add due date')),
           Divider(indent: 68),
-          ListTile(leading: Icon(Icons.event_repeat_outlined), title: Text('Repeat')),
+          ListTile(
+              leading: Icon(Icons.event_repeat_outlined),
+              title: Text('Repeat')),
           Divider(indent: 68),
-          Expanded(child: Container(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 18),
-              child: Text('Add Note'),
-            ),
-          ),)
+          ListTile(
+              leading: Icon(Icons.notes_outlined), title: Text('Add note')),
+          Divider(indent: 68),
         ]),
       ),
     );
--- lib/themes/light.dart
+++ lib/themes/light.dart
@@ -8,11 +8,13 @@ ThemeData lightTheme(ColorScheme? lightDynamic) {
   return ThemeData(
       useMaterial3: true,
       appBarTheme: AppBarTheme(
-        systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
-            systemNavigationBarColor: Colors.transparent),
-        backgroundColor: lightDynamic.onInverseSurface,
-        actionsIconTheme: IconThemeData(color: lightDynamic.onPrimaryContainer),
-        centerTitle: false,
-      ));
+          systemOverlayStyle: const SystemUiOverlayStyle(
+              statusBarColor: Colors.transparent,
+              systemNavigationBarColor: Colors.transparent),
+          backgroundColor: lightDynamic.onInverseSurface,
+          actionsIconTheme:
+              IconThemeData(color: lightDynamic.onPrimaryContainer),
+          centerTitle: false,
+          titleTextStyle:
+              TextStyle(fontSize: 18, color: lightDynamic.inverseSurface)));
 }
